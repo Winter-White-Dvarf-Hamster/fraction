4daf41a1b72a70c72328419132041fa362df58f0
Winter-White-Dvarf-Hamster <antonov.viktor.y@gmail.com>
2025-07-16 20:03:07 +0300

    Add files via upload

---
+++ fruction/Fraction.Test/UnitTest1.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+using Task4Kaba;
+
+namespace Fraction.Test
+{
+    public class Tests
+    {
+        [TestCase("1/2", "1/2")]
+        [TestCase("1/3", "1/3")]
+        [TestCase("1/4", "1/4")]
+        [TestCase("1/5", "1/5")]
+        [TestCase("2/11", "2/11")]
+        [TestCase("21/111", "21/111")]
+        public void FractionParseCorrect(string process, string expected)
+        {
+            var actual = Task4Kaba.Fraction.Parse(process);
+            Assert.IsTrue(actual.ToString() == expected);
+        }
+
+        [TestCase("1/2", "1/2", "1/4")]
+        [TestCase("1/3", "1/3", "1/9")]
+        [TestCase("1/4", "3/4", "3/16")]
+        [TestCase("3/5", "2/5", "6/25")]
+        [TestCase("3/11", "7/11", "21/121")]
+        [TestCase("21/25", "21/25", "441/625")]
+        public void FractionMultFractionCorrect(string left, string right, string expected)
+        {
+            var f1 = Task4Kaba.Fraction.Parse(left);
+            var f2 = Task4Kaba.Fraction.Parse(right);
+            Assert.IsTrue(f1.Mult(f2).ToString() == expected);
+        }
+
+        [TestCase("5/2", 1, "7/2")]
+        [TestCase("6/3", 1, "9/3")]
+        [TestCase("8/4", 1, "12/4")]
+        [TestCase("25/5", 1, "30/5")]
+        [TestCase("22/11", 1, "33/11")]
+        [TestCase("42/21", 1, "63/21")]
+        public void FractionSubIntCorrect(string left, int right, string expected)
+        {
+            var f1 = Task4Kaba.Fraction.Parse(left);
+            var f2 = new Task4Kaba.Fraction(right, 1);
+            Assert.IsTrue(f1.Add(f2).ToString() == expected);
+        }
+
+        [TestCase("1/2", 2, "1/4")]
+        [TestCase("1/3", 2, "1/6")]
+        [TestCase("1/4", 3, "1/12")]
+        [TestCase("3/5", 5, "3/25")]
+        [TestCase("3/11", 7, "3/77")]
+        [TestCase("21/25", 21, "21/525")]
+        public void FractionDivIntCorrect(string left, int right, string expected)
+        {
+            var f1 = Task4Kaba.Fraction.Parse(left);
+            Assert.IsTrue(f1.Div(right).ToString() == expected);
+        }
+
+        [TestCase("1/2", 2, "2/2")]
+        [TestCase("1/3", 2, "2/3")]
+        [TestCase("1/4", 3, "3/4")]
+        [TestCase("3/5", 5, "15/5")]
+        [TestCase("3/11", 7, "21/11")]
+        [TestCase("21/25", 21, "441/25")]
+        public void FractionMultIntCorrect(string left, int right, string expected)
+        {
+            var f1 = Task4Kaba.Fraction.Parse(left);
+            Assert.IsTrue(f1.Mult(right).ToString() == expected);
+        }
+    }
+}
---
+++ fruction/Task4Kaba/Fraction.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4Kaba
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+        public Fraction()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Fraction(int Numerator,int Denominator)
+        {
+            numerator = Numerator;
+            denominator = Denominator;
+        }
+        public Fraction(string str)
+        {
+            numerator = 0;
+            denominator = 0;
+        }
+        public static Fraction Parse(string str)
+        {
+            string[] tmp = str.Split('/');
+            if (tmp.Length != 2)
+                 throw new ArgumentException();
+            return new Fraction(int.Parse(tmp[0]),int.Parse(tmp[1]));
+        }
+        
+        public Fraction Add(Fraction f)
+        {
+            return new Fraction(this.numerator * f.denominator + f.numerator * denominator, denominator * f.denominator);
+        }
+        public Fraction Sub(Fraction f)
+        {
+            return new Fraction(this.numerator * f.denominator - f.numerator * denominator, denominator * f.denominator);
+        }
+        public Fraction Div(Fraction f)
+        {
+            int gcd = GCD(Math.Abs(this.numerator * f.denominator), Math.Abs(denominator * f.numerator));
+            return new Fraction(this.numerator * f.denominator / gcd, denominator * f.numerator / gcd);
+        }
+        public Fraction Div(int val)
+        {
+            int gcd = GCD(Math.Abs(this.numerator), Math.Abs(denominator * val));
+            return new Fraction(this.numerator / gcd, denominator * val / gcd);
+        }
+        public Fraction Mult(Fraction f)
+        {
+            int gcd = GCD(Math.Abs(this.numerator * f.numerator), Math.Abs(denominator * f.denominator));
+            return new Fraction(this.numerator * f.numerator / gcd, denominator * f.denominator / gcd); 
+        }
+        public Fraction Mult(int val)
+        {
+            int gcd = GCD(Math.Abs(this.numerator * val), Math.Abs(denominator));
+            return new Fraction(this.numerator * val / gcd, denominator / gcd);
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+        public double ToDouble()
+        {
+            return (double)numerator / denominator;
+        }
+        public Fraction Reduce()
+        {
+            int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+            return new Fraction(numerator / gcd, denominator / gcd);
+        }
+        private int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+        
+            }
+            return a;
+        }
+    }
+}
